5435bb394b4e31e0503580c0365a995477258c87
Hackdady <lenskii.vm19@physics.msu.ru>
2023-05-02 15:56:06 +0900

    created request

--- Validators_request.ts
+++ Validators_request.ts
@@ -7,19 +7,26 @@ const env = load({
 
 const serverSideKey = env.ServerSideKey; 
 const exampleURL = 'https://swapi.dev/api/';
-const tonapi = "https://tonapi.io/v1/blockchain/validators";
+const tonapiURL = "https://tonapi.io/v1/blockchain/validators";
+type GetResponse = {
+    data:[]
+}
 
-const headers = { 'Authorization': 'Bearer ' + serverSideKey };
-const getValidators = () => {
-    return axios.get(tonapi, { headers: headers });
+console.log(exampleURL);
+const headers = { 'Authorization': 'Bearer ' + serverSideKey, Accept: 'application/json' };
+
+const getValidators = async function () {
+    const { data, status } = await axios.get<GetResponse>(tonapiURL, { headers: headers });
+    console.log(status);
+    return data;
 }
 //const response = axios.get(URL + "starships");
-getValidators()
-    .then(res => {
+console.log(getValidators());
+    /*.then(res => {
         //console.log(res.data);
         console.log(res.data.validators.length);
         console.log(res.data.validators[res.data.validators.length - 1]);
     })
     .catch(err => {
         console.log("Error", err);
-    })
+    })*/
