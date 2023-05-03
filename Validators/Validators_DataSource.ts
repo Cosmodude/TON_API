ed34f86a2b798ea3364d8a51c780a744f44581a5
Hackdady <lenskii.vm19@physics.msu.ru>
2023-05-03 18:22:14 +0900

    Data source added

---
+++ Validators/Validators_DataSource.ts
@@ -0,0 +1,28 @@
+import { DataSource } from "typeorm"
+import { load } from 'ts-dotenv';
+import { Pool } from './pool.entity';
+
+const env = load({
+    DB_USER: String,
+    DB_PASSWORD: String,
+    DB_HOST: String,
+    DB_NAME: String,
+});
+
+const AppDataSource = new DataSource({
+    type: 'mysql',
+    host: env.DB_HOST,
+    port: 3306,
+    username: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
+    entities: [Pool],
+})
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
