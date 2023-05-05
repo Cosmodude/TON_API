153309f389bcb241a3df147259edfd5a2e060452
Hackdady <lenskii.vm19@physics.msu.ru>
2023-05-05 13:47:11 +0900

    renamed the folder

--- Validators_TS/src/Validators_DataSource.ts
+++ Validators_TS/src/Validators_DataSource.ts
@@ -16,7 +16,7 @@ export const ValidatorsDataSource = new DataSource({
     username: env.DB_USER,
     password: env.DB_PASSWORD,
     database: env.DB_NAME,
-    entities: ["entity/*.ts"],
+    entities: ["./entity/*.ts"],
     synchronize: false,
 })
 
