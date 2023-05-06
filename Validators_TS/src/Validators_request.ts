fa8b4b0679963fe865bdbced77e2171a941b71a7
Hackdady <lenskii.vm19@physics.msu.ru>
2023-05-06 21:09:27 +0900

    changed code

--- Validators_TS/src/Validators_request.ts
+++ Validators_TS/src/Validators_request.ts
@@ -8,56 +8,57 @@ const env = load({
     ServerSideKey: String,
 });
 
-
-async function getOurValidatorsAdresses() {
-    await ValidatorsDataSource.initialize();
-    const ourValidators = await ValidatorsDataSource
-        .manager
-        .find(Pool)
-    const adrresses = [];
-    for (let validator in ourValidators) {
-        adrresses.push(validator["address"]);
-    }
-    console.log(ourValidators);
-    console.log( adrresses);
-}
-
-
-const serverSideKey = env.ServerSideKey; 
-const exampleURL = 'https://swapi.dev/api/';
-const tonapiURL = "https://tonapi.io/v1/blockchain/validators";
 type GetResponse = {
-    data:[]
+    data: []
 }
-console.log(exampleURL);
 
-const headers = { 'Authorization': 'Bearer ' + serverSideKey, Accept: 'application/json' };
+export class Validators {
+    
+    async getOurValidatorsAdresses() {
+        await ValidatorsDataSource.initialize();
+        const ourValidators = await ValidatorsDataSource
+            .manager
+            .find(Pool)
+        const adrresses = [];
+        for (let validator in ourValidators) {
+            adrresses.push(validator["address"]);
+        }
+        console.log(ourValidators);
+        console.log(adrresses);
+        return adrresses;
+    }
 
-const getValidators = async function () {
-    const { data, status } = await axios.get<GetResponse>(tonapiURL, { headers: headers });
-    console.log(status);
-    return data;
+    async getValidators() {
+        const serverSideKey = env.ServerSideKey;
+        const exampleURL = 'https://swapi.dev/api/';
+        const tonapiURL = "https://tonapi.io/v1/blockchain/validators";
+
+
+        const headers = { 'Authorization': 'Bearer ' + serverSideKey, Accept: 'application/json' };
+
+        const { data, status } = await axios.get<GetResponse>(tonapiURL, { headers: headers });
+        console.log(status);
+        return data;
+    }
+    
+    //const response = axios.get(URL + "starships");
+    
+    //const pool = new Pool();
+    //pool.name = "Vlad";
+    //console.log(pool);
+
 }
-const validatorsAdrresses = [
-    '-1:f08931ab0f6297ef9fc92f632c3cbcc0a9f25bd5e8a20d2137788a60c61dd2b6',
-    '-1:658587ac24c7cc479728d59c53d741188e593c47fd957e37cc1f1ec3ea385bd8',
-    '-1:bebb60e6baba8f4c655cad64f440c77682f56a430075015b2a250ac3017e7c79',
-]
-//const response = axios.get(URL + "starships");
+
 getValidators()
-    .then(res => {
-        //console.log(res["validators"][1]["address"])
-        const ourValidatorsData = res["validators"].filter(validator =>  validatorsAdrresses.includes(validator["address"]));
-        //console.log(res["validators"]);
-        //console.log(ourValidatorsData);
-        //console.log(res.data.validators[res.data.validators.length - 1]);
-    })
-    .catch(err => {
-        console.log("Error", err);
-    })
+        .then(res => {
+            //console.log(res["validators"][1]["address"])
+            const ourValidatorsData = res["validators"].filter(validator => validatorsAdrresses.includes(validator["address"]));
+            //console.log(res["validators"]);
+            //console.log(ourValidatorsData);
+            //console.log(res.data.validators[res.data.validators.length - 1]);
+        })
+        .catch(err => {
+            console.log("Error", err);
+        })
 
-//const pool = new Pool();
-//pool.name = "Vlad";
-//console.log(pool);
-
-getOurValidatorsAdresses();
+        this.getOurValidatorsAdresses();
